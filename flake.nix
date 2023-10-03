54352485677acb8ff6a0e5bff8d0c2148fc29842
Arsenii Zorin <zorinarseny@yandex.ru>
2023-10-03 23:53:50 +0200

    Uses unstable versions

--- flake.nix
+++ flake.nix
@@ -1,8 +1,10 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    #nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      #url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
