63120c61c131960e933a5d419779eda52e292308
Arseniy Zorin <a.zorin@cs.money>
2023-10-08 20:46:16 +0200

    [slack] An attempt to fix slack in according to https://nixos.wiki/wiki/Slack

--- modules/slack/default.nix
+++ modules/slack/default.nix
@@ -10,7 +10,18 @@ in {
   };
 
   config = mkIf config.my-config.slack.enable {
-    environment.systemPackages = with pkgs; [ slack ];
+    environment.systemPackages = with pkgs; [
+      (slack.overrideAttrs (old: {
+        installPhase = old.installPhase + ''
+          rm $out/bin/slack
+
+          makeWrapper $out/lib/slack/slack $out/bin/slack \
+            --prefix XDG_DATA_DIRS : $GSETTINGS_SCHEMAS_PATH \
+            --prefix PATH : ${lib.makeBinPath [pkgs.xdg-utils]} \
+            --add-flags "--ozone-platform=wayland --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer"
+        '';
+      }))
+    ];
     nixpkgs.allowUnfreePackages = [ "slack" ];
   };
 }
