0ba152f0b988ff2b3149023384d391510aa5ec85
Arseniy Zorin <a.zorin@cs.money>
2023-11-10 14:00:03 +0100

    [starship] Fixes typo

--- modules/starship/default.nix
+++ modules/starship/default.nix
@@ -3,7 +3,7 @@ let inherit (lib) types mkIf mkDefault mkOption;
 in {
   options.my-config = {
     starship.enable = mkOption {
-      description = "Enable my customized git";
+      description = "Enable my customized starship";
       type = types.bool;
       default = false;
     };
