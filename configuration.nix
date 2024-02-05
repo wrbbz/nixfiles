73a4fbb9a126a055b1f92df3c2062bab5b8dce02
Arsenii Zorin <zorinarseny@yandex.ru>
2024-02-05 22:51:11 +0300

    [locale] Uses en_US.utf8 locale

--- configuration.nix
+++ configuration.nix
@@ -95,4 +95,19 @@
 
   networking.nftables.enable = true;
   networking.firewall.enable = true;
+
+  i18n = {
+    defaultLocale = "en_US.utf8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_US.utf8";
+      LC_IDENTIFICATION = "en_US.utf8";
+      LC_MEASUREMENT = "en_US.utf8";
+      LC_MONETARY = "en_US.utf8";
+      LC_NAME = "en_US.utf8";
+      LC_NUMERIC = "en_US.utf8";
+      LC_PAPER = "en_US.utf8";
+      LC_TELEPHONE = "en_US.utf8";
+      LC_TIME = "en_US.utf8";
+    };
+  };
 }
