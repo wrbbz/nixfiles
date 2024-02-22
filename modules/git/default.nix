a184b7b1f8db5fc0980f7f446a7935f1ab7fc017
Arsenii Zorin <zorinarseny@yandex.ru>
2024-02-22 16:56:16 +0300

    feat(git): Adds conditional config

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -13,7 +13,7 @@ in {
     home-manager.users.wrbbz = {
       programs.git = {
         enable = true;
-        userEmail = "a.zorin@cs.money";
+        userEmail = "me@wrbbz.com";
         userName = "Arseniy Zorin";
         aliases = {
           lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n%C(white)%s%C(reset) %C(dim green)- %an%C(reset)' --all";
@@ -30,6 +30,14 @@ in {
           "*.swp"
           ".envrc"
         ];
+        includes = [{
+          condition = "gitdir:~/work/";
+          contents = {
+            user = {
+              email = "a.zorin@cs.money";
+            };
+          };
+        }];
       };
     };
   };
