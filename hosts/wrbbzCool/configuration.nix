1763b45655a171750cfa634bfe01c15a6d005347
Arsenii Zorin <zorinarseny@yandex.ru>
2024-03-01 09:29:33 +0300

    fix(wireplumber): Comments wireplumber configuration. For now

--- hosts/wrbbzCool/configuration.nix
+++ hosts/wrbbzCool/configuration.nix
@@ -18,16 +18,16 @@
   };
   services.emacs.enable = false;
 
-  environment.etc = {
-    "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
-      bluez_monitor.properties = {
-        ["bluez5.enable-sbc-xq"] = true,
-        ["bluez5.enable-msbc"] = true,
-        ["bluez5.enable-hw-volume"] = true,
-        ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-      }
-    '';
-  };
+  # environment.etc = {
+  #   "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+  #     bluez_monitor.properties = {
+  #       ["bluez5.enable-sbc-xq"] = true,
+  #       ["bluez5.enable-msbc"] = true,
+  #       ["bluez5.enable-hw-volume"] = true,
+  #       ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+  #     }
+  #   '';
+  # };
 
   # Allows connect iOS devices
   services.usbmuxd = {
