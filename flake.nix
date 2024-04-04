676947733ae4d33ae4050adba122624f165c7566
Arsenii Zorin <wrbbz@Arseniis-MacBook-Pro.local>
2024-04-04 21:43:26 +0300

    reference-update(flake): updates lock file

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nix-darwin = {
+      url = "github:lnl7/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -10,7 +14,7 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
-  outputs = { self, nixpkgs, home-manager, nixos-hardware }:
+  outputs = { self, nixpkgs, nix-darwin, home-manager, nixos-hardware }:
     let
       mkHost = hostName: system:
         (({ my-config, zfs-root, pkgs, system, ... }:
@@ -62,9 +66,57 @@
             system = system;
             pkgs = nixpkgs.legacyPackages.${system};
           }));
+
+      mkDarwin = hostName: system:
+      (({ my-config, pkgs, system, ... }:
+        nix-darwin.lib.darwinSystem {
+          inherit system;
+          modules = [
+            # Module 0: zfs-root
+            ./modules
+
+            # Module 1: host-specific config, if exist
+            (if (builtins.pathExists
+              ./hosts/${hostName}/configuration.nix) then
+              (import ./hosts/${hostName}/configuration.nix { inherit pkgs nixos-hardware; })
+            else
+              { })
+
+            # Module 2: entry point
+            (({ my-config, pkgs, lib, ... }: {
+              inherit my-config;
+              system.configurationRevision = if (self ? rev) then
+                self.rev
+              else
+                throw "refuse to build: git tree is dirty";
+              system.stateVersion = "23.05";
+              imports = [
+                "${nixpkgs}/nixos/modules/installer/scan/not-detected.nix"
+              ];
+            }) {
+              inherit my-config pkgs;
+              lib = nixpkgs.lib;
+            })
+
+            # Module 3: home-manager
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+            }
+
+            # Module 4: config shared by all hosts
+            (import ./configuration.nix { inherit pkgs; })
+          ];
+        })
+        (import ./hosts/${hostName} {
+          system = system;
+          pkgs = nixpkgs.legacyPackages.${system};
+        }));
     in {
       nixosConfigurations = {
         wrbbzCool = mkHost "wrbbzCool" "x86_64-linux";
+        wrbbzMBook = mkDarwin "wrbbzMBook" "aarch64-darwin";
       };
     };
 }
