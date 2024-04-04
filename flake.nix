60a324219f3181fa1733b4fed97cbf3ea1413256
Arsenii Zorin <me@wrbbz.com>
2024-04-04 22:38:26 +0300

    fix(darwin): drops nixos-hardware

--- flake.nix
+++ flake.nix
@@ -78,7 +78,7 @@
                 # Module 1: host-specific config, if exist
                 (if (builtins.pathExists
                   ./hosts/${hostName}/configuration.nix) then
-                  (import ./hosts/${hostName}/configuration.nix { inherit pkgs nixos-hardware; })
+                  (import ./hosts/${hostName}/configuration.nix { inherit pkgs; })
                 else
                 { })
 
