15853f7d8928e64b16934c75831d531abe648fdc
Arsenii Zorin <me@wrbbz.com>
2024-04-04 22:31:16 +0300

    feat(hm): adds home-manager user

--- hosts/wrbbzMBook/configuration.nix
+++ hosts/wrbbzMBook/configuration.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }: {
 
+  home-manager.users.wrbbz = {
+    home.username = "wrbbz";
+    home.stateVersion = "23.05";
+  };
   environment.systemPackages = with pkgs; [
     glow
     jq
