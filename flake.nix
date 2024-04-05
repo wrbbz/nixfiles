718d16c014b99d8b265f0735934a8e63f18b56da
Arsenii Zorin <me@wrbbz.com>
2024-04-05 16:49:02 +0300

    fix(version): fixes system.stateVersion

--- flake.nix
+++ flake.nix
@@ -89,7 +89,7 @@
                     self.rev
                 else
                     throw "refuse to build: git tree is dirty";
-                system.stateVersion = "23.05";
+                system.stateVersion = 4;
                 # imports = [
                 #     "${nixpkgs}/nixos/modules/installer/scan/not-detected.nix"
                 # ];
