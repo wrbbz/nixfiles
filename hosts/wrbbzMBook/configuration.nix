156d00685da33f4228a48c3f48a95fa0225a6193
Arsenii Zorin <me@wrbbz.com>
2024-04-06 21:01:34 +0300

    fix(hm): drops hm config duplication

--- hosts/wrbbzMBook/configuration.nix
+++ hosts/wrbbzMBook/configuration.nix
@@ -1,11 +1,5 @@
 { pkgs, ... }: {
 
-  home-manager.users.wrbbz = {
-    home = {
-      username = "wrbbz";
-      stateVersion = "23.05";
-    };
-  };
   environment.systemPackages = with pkgs; [
     "pulumi-bin"
   ];
