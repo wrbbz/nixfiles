3a07bc7bb8ebdd0ee213a319b02d029429ed65a3
Arsenii Zorin <me@wrbbz.com>
2024-04-06 13:20:54 +0300

    feat(user): adds user config

--- modules/darwin.nix
+++ modules/darwin.nix
@@ -1,6 +1,12 @@
 { config, lib, pkgs, ... }: {
-services.nix-daemon.enable = true;
-imports = [
-    ./git
-  ];
+  services.nix-daemon.enable = true;
+  imports = [
+      ./git
+    ];
+  users.users.wrbbz = {
+    initialHashedPassword = "!";
+    isNormalUser = true;
+    home = "/Users/wrbbz";
+    shell = pkgs.zsh;
+  };
 }
