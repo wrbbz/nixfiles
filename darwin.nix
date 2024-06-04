073cadb1b2d0b53238fe41af1e26b44487dac12f
Arseniy Zorin <me@wrbbz.com>
2024-06-04 12:47:22 +0300

    feat(darwin): swaps caps and esc

--- darwin.nix
+++ darwin.nix
@@ -27,4 +27,20 @@
     };
   };
 
+  system.keyboard = {
+    enableKeyMapping = true;
+    /* Swaps caps and esc */
+    userKeyMapping = [
+      {
+        HIDKeyboardModifierMappingSrc = 30064771129;
+        HIDKeyboardModifierMappingDst = 30064771113;
+      }
+      {
+        HIDKeyboardModifierMappingSrc = 30064771113;
+        HIDKeyboardModifierMappingDst = 30064771129;
+      }
+    ];
+
+  };
+
 }
