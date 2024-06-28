f9acee1f0ed06ee5ddacf8251b092ad9d1555c93
Arseniy Zorin <me@wrbbz.com>
2024-06-28 13:33:40 +0300

    darwin(dock): changes dock position and sets autohide

--- darwin.nix
+++ darwin.nix
@@ -27,20 +27,25 @@
     };
   };
 
-  system.keyboard = {
-    enableKeyMapping = true;
-    /* Swaps caps and esc */
-    userKeyMapping = [
-      {
-        HIDKeyboardModifierMappingSrc = 30064771129;
-        HIDKeyboardModifierMappingDst = 30064771113;
-      }
-      {
-        HIDKeyboardModifierMappingSrc = 30064771113;
-        HIDKeyboardModifierMappingDst = 30064771129;
-      }
-    ];
+  system = {
+    defaults.dock = {
+      autohide = true;
+      orientation = "right";
+    };
 
+    keyboard = {
+      enableKeyMapping = true;
+      /* Swaps caps and esc */
+      userKeyMapping = [
+        {
+          HIDKeyboardModifierMappingSrc = 30064771129;
+          HIDKeyboardModifierMappingDst = 30064771113;
+        }
+        {
+          HIDKeyboardModifierMappingSrc = 30064771113;
+          HIDKeyboardModifierMappingDst = 30064771129;
+        }
+      ];
+    };
   };
-
 }
