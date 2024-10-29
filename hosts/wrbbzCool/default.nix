86e72d48b0750905fe4ecf3e518b19cdacc4a1ae
Arsenii Zorin <zorinarseny@yandex.ru>
2024-10-29 13:16:29 +0300

    feat(DP): uses DP instead of HDMI

--- hosts/wrbbzCool/default.nix
+++ hosts/wrbbzCool/default.nix
@@ -43,21 +43,21 @@
         ",preferred,auto,auto"
       ];
       workspaces = [
-        "1, monitor:HDMI-A-1, default:true"
-        "2, monitor:HDMI-A-1"
-        "3, monitor:HDMI-A-1"
-        "4, monitor:HDMI-A-1"
-        "5, monitor:HDMI-A-1"
-        "6, monitor:HDMI-A-1"
-        "7, monitor:HDMI-A-1"
-        "8, monitor:HDMI-A-1"
-        "9, monitor:HDMI-A-1"
+        "1, monitor:DP-1, default:true"
+        "2, monitor:DP-1"
+        "3, monitor:DP-1"
+        "4, monitor:DP-1"
+        "5, monitor:DP-1"
+        "6, monitor:DP-1"
+        "7, monitor:DP-1"
+        "8, monitor:DP-1"
+        "9, monitor:DP-1"
       ];
       paperConfig = ''
         ipc = off
         splash = off
         preload = /home/wrbbz/.pictures/bg.png
-        wallpaper = HDMI-A-1:/home/wrbbz/.pictures/bg.png
+        wallpaper = DP-1:/home/wrbbz/.pictures/bg.png
       '';
     };
     joshuto.enable = true;
