fbfed60d3820f9f8836a5c87d1c84db17ac4e1d9
Arsenii Zorin <zorinarseny@yandex.ru>
2024-11-15 11:23:27 +0300

    config(hyprland): actualises config after update

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -295,10 +295,12 @@ in {
               passes = 1;
               new_optimizations = "on";
             };
-            drop_shadow = "yes";
-            shadow_range = 4;
-            shadow_render_power = 3;
-            "col.shadow" = "rgba(1a1a1aee)";
+            shadow = {
+              enabled = true;
+              range = 4;
+              render_power = 3;
+              color = "rgba(1a1a1aee)";
+            };
           };
           animations = {
             enabled = "yes";
