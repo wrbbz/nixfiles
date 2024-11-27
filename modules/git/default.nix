263eaa18d36014bff8bd690e57256c9ccfe2885b
Arseniy Zorin <me@wrbbz.com>
2024-11-27 09:11:21 +0300

    config(git): updates private email

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -17,7 +17,7 @@ in {
       ];
       programs.git = {
         enable = true;
-        userEmail = "me@wrbbz.com";
+        userEmail = "me@wrb.bz";
         userName = "Arseniy Zorin";
         aliases = {
           lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n%C(white)%s%C(reset) %C(dim green)- %an%C(reset)' --all";
