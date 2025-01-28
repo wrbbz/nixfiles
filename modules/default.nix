d83be786a8d9f1d58dd1b6c9af06643e944825eb
Arsenii Zorin <me@wrb.bz>
2025-01-28 20:32:11 +0300

    fix(unfree): moves unfree to default module

--- modules/default.nix
+++ modules/default.nix
@@ -11,6 +11,7 @@
     ./pass
     ./starship
     ./telepresence
+    ./unfree
     ./yamusic
     ./zsh
   ];
--- modules/linux.nix
+++ modules/linux.nix
@@ -16,7 +16,6 @@
     ./steam
     ./tailscale
     ./transmission
-    ./unfree
     ./wofi
   ];
 }
