b83886ea8af6ca0a9ae4b3421548954f3b662875
Arsenii Zorin <me@wrb.bz>
2025-03-21 00:09:14 +0300

    fix(homebrew): temp fix for homebrew

--- flake.nix
+++ flake.nix
@@ -13,7 +13,8 @@
     };
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     nix-homebrew = {
-      url = "github:zhaofengli-wip/nix-homebrew";
+      # url = "github:zhaofengli-wip/nix-homebrew";
+      url = "git+https://github.com/zhaofengli/nix-homebrew?ref=refs/pull/71/merge";
     };
     homebrew-bundle = {
       url = "github:homebrew/homebrew-bundle";
