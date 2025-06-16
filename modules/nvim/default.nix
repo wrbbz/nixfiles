b64ca7a23b455eba4a9b244ca474611dfc5e1482
Arsenii Zorin <me@wrb.bz>
2025-06-16 13:35:48 +0300

    fix(vim): changes upstream for vim

--- modules/nvim/default.nix
+++ modules/nvim/default.nix
@@ -2,19 +2,19 @@
 let
   inherit (lib) types mkIf mkDefault mkOption;
   nvim-spell-ru-utf8-dictionary = builtins.fetchurl {
-    url = "http://ftp.vim.org/vim/runtime/spell/ru.utf-8.spl";
+    url = "https://ftp.nluug.nl/pub/vim/runtime/spell/ru.utf-8.spl";
     sha256 = "eb2d3bd78a2020b30bcc0a7cfebdacebfb7a427581114f66b88a577ae6dac54d";
   };
   nvim-spell-ru-utf8-suggestions = builtins.fetchurl {
-    url = "http://ftp.vim.org/vim/runtime/spell/ru.utf-8.sug";
+    url = "https://ftp.nluug.nl/pub/vim/runtime/spell/ru.utf-8.sug";
     sha256 = "eabd86168ad85d5bfb8068808cf7982bab0374afc299cb49ecc89d71616f393b";
   };
   nvim-spell-en-utf8-dictionary = builtins.fetchurl {
-    url = "http://ftp.vim.org/vim/runtime/spell/en.utf-8.spl";
+    url = "https://ftp.nluug.nl/pub/vim/runtime/spell/en.utf-8.spl";
     sha256 = "fecabdc949b6a39d32c0899fa2545eab25e63f2ed0a33c4ad1511426384d3070";
   };
   nvim-spell-en-utf8-suggestions = builtins.fetchurl {
-    url = "http://ftp.vim.org/vim/runtime/spell/en.utf-8.sug";
+    url = "https://ftp.nluug.nl/pub/vim/runtime/spell/en.utf-8.sug";
     sha256 = "5b6e5e6165582d2fd7a1bfa41fbce8242c72476222c55d17c2aa2ba933c932ec";
   };
 in {
