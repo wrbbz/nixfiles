4afe19d8b825880025386bf2f450b10558c57ad7
Arsenii Zorin <me@wrb.bz>
2025-06-27 09:55:54 +0300

    feat(mbook): uses tailscale from nix repos instead of brew

--- hosts/wrbbzMBook/configuration.nix
+++ hosts/wrbbzMBook/configuration.nix
@@ -6,6 +6,7 @@
 
   environment.systemPackages = with pkgs; [
     cloudflared
+    tailscale
     trivy
     zola
   ];
--- modules/brew/casks.nix
+++ modules/brew/casks.nix
@@ -18,5 +18,4 @@ _:
   "flipper"
   "imazing"
   "keymapp"
-  "tailscale"
 ]
