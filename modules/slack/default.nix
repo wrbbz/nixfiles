8ab9965867b9a60cc8b54e0e6ef4207a9828c818
Arsenii Zorin <me@wrb.bz>
2025-09-28 00:20:23 +0300

    config(slack): an attempt to use home.packages

--- modules/slack/default.nix
+++ modules/slack/default.nix
@@ -11,23 +11,25 @@ in {
   };
 
   config = mkIf config.my-config.slack.enable {
-    environment.systemPackages = with pkgs; [
-      (if pkgs.stdenv.isLinux then
-        (slack.overrideAttrs (old: {
-          nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ pkgs.makeWrapper ];
-          # append to postInstall to avoid clobbering upstream phases
-          postInstall = (old.postInstall or "") + ''
-            rm -f $out/bin/slack
+    home-manager.users.wrbbz = {
+      home.packages = with pkgs; [
+        (if pkgs.stdenv.isLinux then
+          (slack.overrideAttrs (old: {
+            nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ pkgs.makeWrapper ];
+            # append to postInstall to avoid clobbering upstream phases
+            postInstall = (old.postInstall or "") + ''
+              rm -f $out/bin/slack
 
-            makeWrapper $out/lib/slack/slack $out/bin/slack \
-              --prefix XDG_DATA_DIRS : $GSETTINGS_SCHEMAS_PATH \
-              --prefix PATH : ${lib.makeBinPath [ pkgs.xdg-utils ]} \
-              --add-flags "--ozone-platform=wayland --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer"
-          '';
-        }))
-      else
-        slack)
-    ];
+              makeWrapper $out/lib/slack/slack $out/bin/slack \
+                --prefix XDG_DATA_DIRS : $GSETTINGS_SCHEMAS_PATH \
+                --prefix PATH : ${lib.makeBinPath [ pkgs.xdg-utils ]} \
+                --add-flags "--ozone-platform=wayland --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer"
+            '';
+          }))
+        else
+          slack)
+      ];
+    };
 
     nixpkgs.allowUnfreePackages = [ "slack" ];
   };
