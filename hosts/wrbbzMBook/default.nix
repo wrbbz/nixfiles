b248edbf27ee8c573582b3d85285efa3b485f8de
Arsenii Zorin <me@wrb.bz>
2025-10-14 19:17:21 +0300

    config(mbook): temporaly disables firefox

--- hosts/wrbbzMBook/default.nix
+++ hosts/wrbbzMBook/default.nix
@@ -7,7 +7,7 @@
     brew.enable = true;
     cli.enable = true;
     dev.enable = true;
-    firefox.enable = true;
+    firefox.enable = false;
     git = {
       enable = true;
       signing = {
