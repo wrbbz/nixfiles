086e9b7c5be52604ac60081634fe4633d167730f
Arsenii Zorin <me@wrb.bz>
2025-12-11 11:56:33 +0300

    config(firefox): adds vimium plugin

--- modules/firefox/default.nix
+++ modules/firefox/default.nix
@@ -58,6 +58,11 @@ in {
               install_url = "https://addons.mozilla.org/firefox/downloads/latest/sidebery/latest.xpi";
               installation_mode = "force_installed";
             };
+            # Vimium
+            "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-ff/latest.xpi";
+              installation_mode = "force_installed";
+            };
           };
         };
       };
