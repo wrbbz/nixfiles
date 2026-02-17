2c63183924af6ac234297b18e19d37da885e478d
Arsenii Zorin <me@wrb.bz>
2026-02-17 17:07:01 +0300

    config(nvim): rename extraLuaConfig to initLua

--- modules/nvim/default.nix
+++ modules/nvim/default.nix
@@ -97,7 +97,7 @@ in {
 
           nnoremap <leader>pv :Explore<cr>
         '';
-        extraLuaConfig = ''
+        initLua = ''
           vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
           vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
